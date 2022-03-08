fd053b2db8f52a9fdba7565f3d2f993405cdc827
mfoots <mfoots@gmail.com>
2022-03-08 03:30:35 -0600

    changes made

--- image.py
+++ image.py
@@ -1,17 +1,11 @@
 from tkinter import *
 from tkinter import ttk
-from turtle import bgcolor
-# from PIL import ImageTk, Image
-
-
 
 window = Tk()
 window.geometry('600x400')
 window.title("Picture This!")
 window.anchor(CENTER)
 
-
-# photo = ImageTk.PhotoImage(Image.open("Atari-2600.png"))
 photo = PhotoImage(file="Atari-2600.png")
 
 label = ttk.Label(window, image=photo)
